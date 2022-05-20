030dda039774ef9b7dae7067c7cff1490802267f
aayushsapkota1 <kodaayush@gmail.com>
2022-05-20 12:08:16 +0545

    Login frontEnd

--- src/Component/redux/reducers/Auth.js
+++ src/Component/redux/reducers/Auth.js
@@ -1,14 +1,19 @@
-import * as actionType from "../constants/actionTypes";
+import * as actionType from '../constants/actionTypes';
 
 const authReducer = (state = { authData: null }, action) => {
   switch (action.type) {
     case actionType.AUTH:
-      localStorage.setItem("profile", JSON.stringify({ ...action?.data }));
+      localStorage.setItem('profile', JSON.stringify({ ...action?.data }));
       return { ...state, authData: action.data };
+    case actionType.LOGOUT:
+      localStorage.removeItem('profile')
+      return { ...state, authData: null };
 
     case actionType.ERROR_AUTH_SIGNUP:
       return { ...state, errorAuthSignUp: action.payload.errorAuthSignUp };
 
+    case actionType.ERROR_AUTH_SIGNIN:
+      return { ...state, errorAuthSignIn: action.payload.errorAuthSignIn };
     default:
       return state;
   }
