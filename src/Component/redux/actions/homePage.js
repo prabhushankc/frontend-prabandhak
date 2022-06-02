bd66c550131678910896d255f95b9aeea41e5cd6
aayushsapkota1 <kodaayush@gmail.com>
2022-06-02 15:57:40 +0545

    Update information in homepage

--- src/Component/redux/actions/homePage.js
+++ src/Component/redux/actions/homePage.js
@@ -1,6 +1,13 @@
-import * as api from '../api';
-import { FETCH_HOMEPAGE, CREATE_HOMEPAGE, START_HOMEPAGE, END_HOMEPAGE, DELETE_HOMEPAGE, UPDATE_HOMEPAGE } from '../constants/actionTypes';
-import { NotifyError, NotifySuccess } from './notify';
+import * as api from "../api";
+import {
+  FETCH_HOMEPAGE,
+  CREATE_HOMEPAGE,
+  START_HOMEPAGE,
+  END_HOMEPAGE,
+  DELETE_HOMEPAGE,
+  UPDATE_HOMEPAGE,
+} from "../constants/actionTypes";
+import { NotifyError, NotifySuccess } from "./notify";
 
 export const fetchHomePage = () => async (dispatch) => {
   try {
@@ -15,12 +22,14 @@ export const fetchHomePage = () => async (dispatch) => {
       NotifyError(error.message);
     }
   }
-}
+};
 
 export const createHomePage = (formData) => async (dispatch) => {
   try {
     dispatch({ type: START_HOMEPAGE });
-    const { data: { savedHomePage, message } } = await api.createHomePage(formData);
+    const {
+      data: { savedHomePage, message },
+    } = await api.createHomePage(formData);
     dispatch({ type: CREATE_HOMEPAGE, payload: { savedHomePage } });
     NotifySuccess(message);
     dispatch({ type: END_HOMEPAGE });
@@ -31,12 +40,30 @@ export const createHomePage = (formData) => async (dispatch) => {
       NotifyError(error.message);
     }
   }
-}
-
+};
+export const updateHomePage = (id, formData) => async (dispatch) => {
+  try {
+    dispatch({ type: START_HOMEPAGE });
+    const {
+      data: { updateHomePage, message },
+    } = await api.updateHomePage(id, formData);
+    dispatch({ type: UPDATE_HOMEPAGE, payload: { updateHomePage } });
+    NotifySuccess(message);
+    dispatch({ type: END_HOMEPAGE });
+  } catch (error) {
+    if (error.response.status >= 400 && error.response.status <= 500) {
+      NotifyError(error.response.data.message);
+    } else {
+      NotifyError(error.message);
+    }
+  }
+};
 export const deleteHome = (id) => async (dispatch) => {
   try {
-    const { data: { message } } = await api.deleteHome(id);
-    dispatch({ type: DELETE_HOMEPAGE, payload: id })
+    const {
+      data: { message },
+    } = await api.deleteHome(id);
+    dispatch({ type: DELETE_HOMEPAGE, payload: id });
     NotifySuccess(message);
   } catch (error) {
     if (error.response.status >= 400 && error.response.status <= 500) {
@@ -45,4 +72,4 @@ export const deleteHome = (id) => async (dispatch) => {
       NotifyError(error.message);
     }
   }
-}
+};
